c3132acfd424cb5f8adee7869d8773240a8efa23
ArsShon2000 <arsenkaribaev98@gmail.com>
2023-06-30 12:44:01 +0300

    Комментарий

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000                                           
 const app = express()
 app.use(cors())
 app.use(express.json())                                                             // ЧТОБЫ ПАРСИТЬ JSON
-app.use(express.static(path.resolve(__dirname, 'static')))                                                           // ЧТОБЫ МОЖНО БЫЛО ПОСМОТРЕТЬ ФАЙЛЫ ИЗ ПАПКИ static
+app.use(express.static(path.resolve(__dirname, 'static')))                          // ЧТОБЫ МОЖНО БЫЛО ПОСМОТРЕТЬ ФАЙЛЫ ИЗ ПАПКИ static
 app.use(fileUpload({}))                                                             // ДЛЯ РАБОТЫ С ФАЙЛАМИ
 // ############### АПИ РЕСТЫ ############### 
 app.use('/api', router)
